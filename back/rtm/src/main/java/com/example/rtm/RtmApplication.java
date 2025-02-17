5fdc2e7f9f26194047b304581215691fc3eb23bd
wochae <officialwochae@gmail.com>
2025-02-17 13:43:43 +0900

    [init] : back init #1

---
+++ back/rtm/src/main/java/com/example/rtm/RtmApplication.java
@@ -0,0 +1,13 @@
+package com.example.rtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtmApplication.class, args);
+	}
+
+}
---
+++ back/rtm/src/test/java/com/example/rtm/RtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
