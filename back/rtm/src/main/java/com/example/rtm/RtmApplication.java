f639d573daf0796fc77d7b18be08d4d36d2b5343
wochae <officialwochae@gmail.com>
2025-02-17 15:48:00 +0900

    [init] : front init #2

--- back/rtm/src/main/java/com/example/rtm/RtmApplication.java
+++ back/rtm/src/main/java/com/example/rtm/RtmApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RtmApplication {
-
+public class RtmApplication {// test
 	public static void main(String[] args) {
 		SpringApplication.run(RtmApplication.class, args);
 	}
